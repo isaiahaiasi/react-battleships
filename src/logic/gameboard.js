3fbf53f0a030c795fbb33aae3c0e485013c27ea3
Isaiah <computerblender@gmail.com>
2021-04-13 00:05:41 -0700

    Add ability for gameboard to receive hits (not passing to ships yet)

--- src/logic/gameboard.js
+++ src/logic/gameboard.js
@@ -1,7 +1,18 @@
 import { vec2 } from "../direction";
 
-export default function gameboard(length, boardArray = []) {
+// I want to make the full constructor private
+export default function gameboardCaller(length) {
+  return gameboard(length);
+}
+
+function gameboard(length, boardArray = [], ships = [], misses = []) {
   const getBoardIndex = (pos) => pos.y * length + pos.x;
+
+  const posOutOfBounds = (pos) =>
+    pos.x < 0 || pos.x > length - 1 || pos.y < 0 || pos.y > length - 1;
+
+  // I'm pretty sure this is a "reducible" functional pattern
+  const getCoords = (pos) => boardArray[getBoardIndex(pos)];
 
   const addShip = (ship, origin, orientation) => {
     const newBoardArray = [...boardArray];
@@ -11,18 +22,43 @@ export default function gameboard(length, boardArray = []) {
         origin.y + orientation.y * i
       );
 
-      if (pos.x < 0 || pos.x > length - 1 || pos.y < 0 || pos.y > length - 1) {
+      if (posOutOfBounds(pos)) {
         throw new Error(
           `Tried to add ship at illegal board position(${pos.x}, ${pos.y})`
         );
       }
       const index = getBoardIndex(pos);
-      newBoardArray[index] = ship;
+
+      // Don't like this, but this seems like the simplest way to hold this info
+      newBoardArray[index] = { ship, i };
     }
-    return gameboard(length, newBoardArray);
+    return gameboard(length, newBoardArray, [...ships, ship], misses);
   };
 
-  const getCoords = (pos) => boardArray[getBoardIndex(pos)];
+  const receiveHit = (pos) => {
+    if (posOutOfBounds(pos)) {
+      throw new Error(
+        `Tried to receive hit at illegal board position (${pos.x}, ${pos.y})`
+      );
+    }
 
-  return { addShip, getCoords, boardArray };
+    const posInfo = boardArray[getBoardIndex(pos)];
+
+    if (posInfo) {
+      const newShip = posInfo.ship.hit(posInfo.i);
+      // get new ships array
+      const newShips = ships.filter((ship) => ship !== posInfo.ship);
+      console.log(newShips);
+      newShips.push(newShip);
+      // return new board
+      return gameboard(length, boardArray, newShips, misses);
+    } else {
+      const newMisses = [...misses, pos];
+      return gameboard(length, boardArray, ships, newMisses);
+    }
+  };
+
+  const isEveryShipSunk = () => ships.every((ship) => ship.isSunk());
+
+  return { addShip, getCoords, receiveHit, misses, isEveryShipSunk };
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,82 +2,8 @@ import board from "../logic/gameboard";
 import ship from "../logic/ship";
 import dir, { vec2 } from "../direction";
 
-test("added ships should be visible at their coordinates (facing down)", () => {
-  // test a ship pointing down
-  const testShip = ship(3);
-  let testBoard = board(4);
-  testBoard = testBoard.addShip(testShip, vec2(0, 0), dir.down);
-
-  const expectedPositions = [vec2(0, 0), vec2(0, 1), vec2(0, 2)];
-  const unexpectedPositions = [
-    vec2(1, 0), // right of where it should be
-    vec2(0, 3), // past where it should be
-  ];
-
-  expectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toEqual(testShip);
-  });
-
-  unexpectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toBeUndefined();
-  });
-});
-
-test("added ships should be visible at their coordinates (facing right)", () => {
-  // test a ship pointing down
-  const testShip = ship(4);
-  let testBoard = board(5);
-  testBoard = testBoard.addShip(testShip, vec2(1, 1), dir.right);
-
-  const expectedPositions = [vec2(1, 1), vec2(2, 1), vec2(3, 1), vec2(4, 1)];
-  const unexpectedPositions = [vec2(0, 1), vec2(1, 0)];
-
-  expectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toEqual(testShip);
-  });
-
-  unexpectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toBeUndefined();
-  });
-});
-
-test("added ships should be visible at their coordinates (facing up)", () => {
-  // test a ship pointing down
-  const testShip = ship(3);
-  let testBoard = board(5);
-  testBoard = testBoard.addShip(testShip, vec2(4, 3), dir.up);
-
-  const expectedPositions = [vec2(4, 3), vec2(4, 2), vec2(4, 1)];
-  const unexpectedPositions = [vec2(0, 3), vec2(4, 0)];
-
-  expectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toEqual(testShip);
-  });
-
-  unexpectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toBeUndefined();
-  });
-});
-
-test("added ships should be visible at their coordinates (facing left)", () => {
-  // test a ship pointing down
-  const testShip = ship(3);
-  let testBoard = board(5);
-  testBoard = testBoard.addShip(testShip, vec2(4, 3), dir.left);
-
-  const expectedPositions = [vec2(4, 3), vec2(3, 3), vec2(2, 3)];
-  const unexpectedPositions = [vec2(0, 3), vec2(4, 0)];
-
-  expectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toEqual(testShip);
-  });
-
-  unexpectedPositions.forEach((pos) => {
-    expect(testBoard.getCoords(pos)).toBeUndefined();
-  });
-});
-
-test("should throw error when attempting to add ship outside array bounds", () => {
+// this one is always ~10x slower than the others, and I have no idea why
+test("should throw error when adding ship outside array bounds", () => {
   const testShip = ship(3);
   let testBoard = board(5);
   expect(() => {
@@ -85,6 +11,104 @@ test("should throw error when attempting to add ship outside array bounds", () =
   }).toThrowError();
 });
 
-xtest("should be able to receive hit", () => {});
+test("should be able to receive hit", () => {
+  const testBoard = board(3);
+  const testBoardHit = testBoard.receiveHit(vec2(0, 0));
+  expect(testBoardHit).not.toBe(testBoard);
+  expect(testBoardHit.misses.length).toBe(1);
+});
 
-xtest("should report if all ships have been sunk", () => {});
+test("should throw error when attempting to hit outside array bounds", () => {
+  const testBoard = board(3);
+  expect(() => {
+    testBoard.receiveHit(vec2(4, 0));
+  }).toThrowError();
+});
+
+test("isEveryShipSunk() should not give false positives", () => {
+  const testBoard = board(3).addShip(ship(3), vec2(0, 0), dir.right);
+
+  expect(testBoard.isEveryShipSunk()).toBe(false);
+});
+
+test("isEveryShipSunk() should return true when all ships are sunk", () => {
+  const testBoard = board(3)
+    .addShip(ship(3), vec2(0, 0), dir.right)
+    .receiveHit(vec2(0, 0))
+    .receiveHit(vec2(1, 0))
+    .receiveHit(vec2(2, 0));
+
+  expect(testBoard.isEveryShipSunk()).toBe(true);
+});
+
+// TODO: might remove all of these tests
+// b/c I don't think I actually need getCoords
+describe("added ships are visible at their coordinates", () => {
+  test("when facing down", () => {
+    const testShip = ship(3);
+    let testBoard = board(4);
+    testBoard = testBoard.addShip(testShip, vec2(0, 0), dir.down);
+
+    const expectedPositions = [vec2(0, 0), vec2(0, 1), vec2(0, 2)];
+    const unexpectedPositions = [vec2(1, 0), vec2(0, 3)];
+
+    expectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos).ship).toEqual(testShip);
+    });
+
+    unexpectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos)).toBeUndefined();
+    });
+  });
+
+  test("when facing right", () => {
+    const testShip = ship(4);
+    let testBoard = board(5);
+    testBoard = testBoard.addShip(testShip, vec2(1, 1), dir.right);
+
+    const expectedPositions = [vec2(1, 1), vec2(2, 1), vec2(3, 1), vec2(4, 1)];
+    const unexpectedPositions = [vec2(0, 1), vec2(1, 0)];
+
+    expectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos).ship).toEqual(testShip);
+    });
+
+    unexpectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos)).toBeUndefined();
+    });
+  });
+
+  test("when facing up", () => {
+    const testShip = ship(3);
+    let testBoard = board(5);
+    testBoard = testBoard.addShip(testShip, vec2(4, 3), dir.up);
+
+    const expectedPositions = [vec2(4, 3), vec2(4, 2), vec2(4, 1)];
+    const unexpectedPositions = [vec2(0, 3), vec2(4, 0)];
+
+    expectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos).ship).toEqual(testShip);
+    });
+
+    unexpectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos)).toBeUndefined();
+    });
+  });
+
+  test("when facing left", () => {
+    const testShip = ship(3);
+    let testBoard = board(5);
+    testBoard = testBoard.addShip(testShip, vec2(4, 3), dir.left);
+
+    const expectedPositions = [vec2(4, 3), vec2(3, 3), vec2(2, 3)];
+    const unexpectedPositions = [vec2(0, 3), vec2(4, 0)];
+
+    expectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos).ship).toEqual(testShip);
+    });
+
+    unexpectedPositions.forEach((pos) => {
+      expect(testBoard.getCoords(pos)).toBeUndefined();
+    });
+  });
+});
