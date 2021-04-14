780b34873b22c7e5ccdec64b4da1285fdf1ca1a9
Isaiah <computerblender@gmail.com>
2021-04-13 22:01:40 -0700

    Add check for overlapping ships, refactor position validation functions

--- src/logic/gameboard.js
+++ src/logic/gameboard.js
@@ -1,5 +1,3 @@
-import vec2 from "../vec2";
-
 // I want to make the full constructor private
 export default function gameboardCaller(length) {
   return gameboard(length);
@@ -9,23 +7,17 @@ function gameboard(length, ships = [], misses = []) {
   const posOutOfBounds = (pos) =>
     pos.x < 0 || pos.x > length - 1 || pos.y < 0 || pos.y > length - 1;
 
-  const addShip = (ship, origin, orientation) => {
-    const positions = [];
-    for (let i = 0; i < ship.length; i++) {
-      const pos = vec2(
-        origin.x + orientation.x * i,
-        origin.y + orientation.y * i
-      );
+  const posContainsShip = (pos) =>
+    ships.some((ship) => ship.getBoardSpaceCoords().some((v) => v.equals(pos)));
 
-      if (posOutOfBounds(pos)) {
-        throw new Error(
-          `Tried to add ship at illegal board position(${pos.x}, ${pos.y})`
-        );
-      }
-      positions[i] = pos;
+  const isValidShipPos = (pos) => !posOutOfBounds(pos) && !posContainsShip(pos);
+
+  const addShip = (ship) => {
+    if (ship.getBoardSpaceCoords().some((pos) => !isValidShipPos(pos))) {
+      throw new Error(`Tried to add ship at illegal board position`);
     }
 
-    return gameboard(length, [...ships, { ship, positions }], misses);
+    return gameboard(length, [...ships, ship], misses);
   };
 
   const receiveHit = (hitPos) => {
@@ -37,19 +29,20 @@ function gameboard(length, ships = [], misses = []) {
 
     // try to find a ship at that position
     const hitShip = ships.find((ship) =>
-      ship.positions.some((position) => position.equals(hitPos))
+      ship.getBoardSpaceCoords().some((position) => position.equals(hitPos))
     );
 
     if (hitShip) {
       // Find which segment of the ship was hit by matching board-space coords
-      const hitSegment = hitShip.positions.findIndex((position) =>
-        position.equals(hitPos)
-      );
+      // TODO: this should probably be handled by ship...
+      const hitSegment = hitShip
+        .getBoardSpaceCoords()
+        .findIndex((pos) => pos.equals(hitPos));
 
       // remove hitship from ships and replace with newship
-      const newShip = hitShip.ship.hit(hitSegment);
+      const newShip = hitShip.hit(hitSegment);
       const newShips = [...ships].splice(0, ships.indexOf(hitShip), 1);
-      newShips.push({ ship: newShip, positions: hitShip.positions });
+      newShips.push(newShip);
 
       return gameboard(length, newShips, misses);
     } else {
@@ -58,7 +51,7 @@ function gameboard(length, ships = [], misses = []) {
     }
   };
 
-  const isEveryShipSunk = () => ships.every((ship) => ship.ship.isSunk());
+  const isEveryShipSunk = () => ships.every((ship) => ship.isSunk());
 
-  return { addShip, receiveHit, misses, isEveryShipSunk };
+  return { addShip, receiveHit, misses, isEveryShipSunk, isValidShipPos };
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -17,35 +17,36 @@ test("should throw error when attempting to hit outside array bounds", () => {
 });
 
 test("isEveryShipSunk() should not give false positives", () => {
-  const testBoard = board(3).addShip(ship(3), vec2(0, 0), dir.right);
+  const testBoard = board(3).addShip(ship(3, vec2(0, 0), dir.right));
 
   expect(testBoard.isEveryShipSunk()).toBe(false);
 });
 
 test("isEveryShipSunk() should return true when all ships are sunk", () => {
   const testBoard = board(3)
-    .addShip(ship(3), vec2(0, 0), dir.right)
+    .addShip(ship(3, vec2(0, 0), dir.right))
     .receiveHit(vec2(0, 0))
     .receiveHit(vec2(1, 0))
     .receiveHit(vec2(2, 0))
-    .addShip(ship(2), vec2(0, 1), dir.down)
+    .addShip(ship(2, vec2(0, 1), dir.down))
     .receiveHit(vec2(0, 1))
     .receiveHit(vec2(0, 2));
 
   expect(testBoard.isEveryShipSunk()).toBe(true);
 });
 
-//!--------
-// TODO: "isValidShipPos" should be its own function, and it shouldn't throw
-// It's good to check in addShip, but that's not the ONLY place I want it!
-// (ie, user should be able to see if a pos is valid before attempting to place)
-xtest("should throw error when placing a ship on top of another ship", () => {});
+test("should throw error when placing a ship on top of another ship", () => {
+  expect(() =>
+    board(5)
+      .addShip(ship(4, vec2(4, 4), dir.down))
+      .addShip(ship(5, vec2(4, 4), dir.right))
+  ).toThrowError();
+});
 
 // this one is always ~10x slower than the others, and I have no idea why
 test("should throw error when adding ship outside array bounds", () => {
-  const testShip = ship(3);
   let testBoard = board(5);
   expect(() => {
-    testBoard.addShip(testShip, vec2(4, 0), dir.right);
+    testBoard.addShip(ship(3, vec2(4, 3), dir.down));
   }).toThrowError();
 });
