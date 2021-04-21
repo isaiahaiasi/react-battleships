33a2a60a034d4b1c7ecd9cc19de7a2d2686b63b7
isaiahaiasi <76509656+isaiahaiasi@users.noreply.github.com>
2021-04-21 01:15:08 -0700

    Delete player.js

--- src/logic/player.js
+++
@@ -1,7 +0,0 @@
-// my "turn manager"
-const getCurrentPlayer = (players, turn) => players[turn % players.length];
-
-const takeTurn = (boardState, pos, updater) =>
-  updater(() => boardState.receiveHit(pos));
-
-export { getCurrentPlayer, takeTurn };
